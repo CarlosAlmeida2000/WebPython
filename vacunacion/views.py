4fb4b5725257e869ebbb9c6f917ec93eca135e06
CarlosAlmeida2000 <ca884012@gmail.com>
2021-08-28 14:56:52 -0500

    Corrgiendo la ApiView Vacunación

--- vacunacion/views.py
+++ vacunacion/views.py
@@ -1,28 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from vacunacion.models import *
-
+import json
 
 class Vacunacion(APIView):
-    def post(self, request, format=None):
-        if request.method == "POST":
+    def post(self, request, format = None):
+        if request.method == 'POST':
             try:
-                cedula = request.POST["cedula"]
-                nombres = request.POST["nombres"].split()
+                json_data = json.loads(request.body.decode('utf-8'))
+                cedula = json_data['cedula']
+                nombres = (json_data['nombres']).split()
                 nombre1 = nombres[0]
                 nombre2 = nombres[1]
                 apellido1 = nombres[2]
                 apellido2 = nombres[3]
-                unCiudadano = ciudadanos.objects.get(cedula=cedula, nombre1=nombre1, nombre2=nombre2, apellido1=apellido1, apellido2=apellido2)
+                unCiudadano = ciudadanos.objects.get(cedula = cedula, nombre1__icontains = nombre1, nombre2__icontains = nombre2, apellido1__icontains = apellido1, apellido2__icontains = apellido2)
                 json_consulta = {
-                    "nombres": request.POST["nombres"],
+                    "nombres": json_data['nombres'].upper(),
                     "provincia": unCiudadano.provincia.provincia,
                     "canton": unCiudadano.canton.canton,
                     "centro_vacunacion": unCiudadano.centroVacunacion.centro_vacunacion,
                     "direccion": unCiudadano.centroVacunacion.direccion,
                     "primera_dosis": unCiudadano.primeraDosis,
-                    "segunda_dosis": unCiudadano.segundaDosis,
+                    "segunda_dosis": unCiudadano.segundaDosis
                 }
-                return Response({"consulta": json_consulta})
+                return Response({"consulta": json_consulta})    
             except ciudadanos.DoesNotExist:
                 return Response({"mensaje": "Ups, no se encuentra registrado...."})
